7a2d7fa4522d519d543f34026699bb322949e349
Olamihaelov <olamihaelov@gmail.com>
2025-07-29 21:00:47 +0300

    Create 3 ON DELETE CASCADE.sql

---
+++ 3 ON DELETE CASCADE.sql
@@ -0,0 +1,37 @@
+-- ON DELETE CASCADE | SQLite
+
+CREATE TABLE directors (
+    director_id INTEGER PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE movies (
+    movie_id INTEGER PRIMARY KEY,
+    title TEXT,
+    director_id INTEGER,
+    FOREIGN KEY(director_id) REFERENCES directors(director_id)
+);
+
+INSERT INTO directors (name) VALUES ('Spielberg');
+INSERT INTO movies (title, director_id) VALUES ('Jaws', 1);
+
+DELETE FROM directors WHERE name = 'Spielberg';
+
+DROP TABLE movies;
+
+CREATE TABLE movies (
+    movie_id INTEGER PRIMARY KEY,
+    title TEXT,
+    director_id INTEGER,
+    FOREIGN KEY(director_id) REFERENCES directors(director_id) ON DELETE CASCADE
+);
+
+
+INSERT INTO movies (title, director_id) VALUES ('Jaws', 1);
+
+
+DELETE FROM directors WHERE name = 'Spielberg';
+
+
+SELECT * FROM directors;
+SELECT * FROM movies;
